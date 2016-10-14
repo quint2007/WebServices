e32fb00fcd6cd8f406eb9245a002637a0bf481cb
AHelbig <quint0095@gmail.com>
2016-10-14 11:44:10 +0200

    DailyCommit

---
+++ CalcServices/CalcServices/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CalcService
+{
+    public class Calculator : ICalculator
+    {
+        public Result Add(Arguments args)
+        {
+            return new Result {Value = args.Arg1 + args.Arg2};
+        }
+    }
+}
--- CalcServices/CalcServices/Program.cs
+++ CalcServices/CalcServices/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,17 @@ namespace CalcService
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            var serviceHost = new ServiceHost(typeof(Calculator));
+            serviceHost.Open();
+
+            Console.WriteLine("Service started");
+            Console.WriteLine("Press any key to close");
+            Console.Read();
+
+            serviceHost.Close();
         }
     }
 }
